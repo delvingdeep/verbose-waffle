c5eaa5b04a4cb47cd9dc231f759b77d974619ab0
Deep Doshi <deepdoshi@live.in>
2018-07-26 14:10:40 -0400

    Add frame data logging code snippets

--- camera_logging.py
+++ camera_logging.py
@@ -9,12 +9,20 @@ Author: Deep Doshi
 
 
 # import libraries
+import csv
 import cv2
+import pandas as pd
 
 # output image directory
-output_video = "path_to_directory"
+output_test = "path_to_directory"
 
 count = 0  # variable to count frames for logging
+
+# dimensions to crop the image frame
+y1=0
+x1=0
+y2=500  
+x2=500  
 
 # read video feed from any USB camera
 feed = cv2.VideoCapture(0)      # 0 denotes in-built Webcam
@@ -30,9 +38,31 @@ while True:
     # show run-time video feed on console
     cv2.imshow("input", frame)
     
-    # write image frame to local system
-    cv2.imwrite(output_video + "/%#05d.jpg" % (count+1), frame)
+    # crop image to user-defined size
+    cropped_image = frame[y1:y2,x1:x2]
+    
+    # write frame to the local system
+    cv2.imwrite(output + "/%#05d.jpg" % (count+1), cropped_image)
+    
+    # get path of the saved frame
+    path = output + "/%#05d.jpg" % (count+1)
+    
     count = count + 1
+    
+    # get time from system time for each frame logging
+    hour = pd.datetime.now().hour
+    minute = pd.datetime.now().minute
+    second = pd.datetime.now().second
+    millisecond = int(pd.datetime.now().microsecond / 1000)
+    
+    time_ms = str(hour) + ":" + str(minute) + ":" + str(second) + ":" + str(millisecond)
+    
+    # csv write
+    data = [path, time_ms]
+    
+    with open(r'camera_log.txt','a') as wrt:
+        writer = csv.writer(wrt)
+        writer.writerow(data)
     
     # exit video feed on ESC keystroke
     e = cv2.waitKey(1)
